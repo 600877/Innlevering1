e2175ef1db925878ef67318fb3ea35c43d38feb1
Frida <Frida@LAPTOP-R8KQ54QN.lan>
2021-09-10 17:50:15 +0200

    Innlevering 1

--- Innlevering/src/OppgaveO2.java
+++ Innlevering/src/OppgaveO2.java
@@ -1,9 +1,36 @@
+import static javax.swing.JOptionPane.showInputDialog;
 
 public class OppgaveO2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		for (int i = 0; i < 10; i++) {
 
+			int poeng = -1;
+			
+			while (poeng < 0 || poeng > 100) {
+				poeng = Integer.parseInt(showInputDialog("Poeng:"));
+				if (poeng < 0 || poeng > 100)
+					System.out.println("Ugyldig poeng");
+			}
+			String karakter = "";
+			
+			if (poeng >= 90)
+				karakter = "A";
+			else if (poeng >= 80)
+				karakter = "B";
+			else if (poeng >= 60)
+				karakter = "C";
+			else if (poeng >= 50)
+				karakter = "D";
+			else if (poeng >= 40)
+				karakter = "E";
+			else 
+				karakter = "F";
+			
+			System.out.println("Karakter: " + karakter);
+		}
 	}
 
 }
--- Innlevering/src/OppgaveO3.java
+++ Innlevering/src/OppgaveO3.java
@@ -1,8 +1,22 @@
+import static javax.swing.JOptionPane.showInputDialog;
 
 public class OppgaveO3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		int n = Integer.parseInt(showInputDialog("n:"));
+		if (n <= 0) {
+			System.out.println("Ugyldig tall");
+			return;
+		}
+		int fakultet = 1;
+		
+//		
+		for (int i = 2; i <= n; i++) {
+			fakultet = fakultet * i;
+		}
+		System.out.println("n! = " + fakultet);
 
 	}
 
